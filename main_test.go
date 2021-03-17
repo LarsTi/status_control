9fe9939d6f906cfdc9b01e6bbdb287e3f5de4e27
Lars Ti <lars.tilsner@googlemail.com>
2021-03-18 00:49:22 +0100

    update go.mod

--- main_test.go
+++ main_test.go
@@ -2,7 +2,7 @@ package main
 import(
 	"testing"
 )
-func TestReadEnv(t *testingi.T) {
+func TestReadEnv(t *testing.T) {
 	got := readEnv()
 	if len(got) != 0 {
 		t.Errorf("len(got) = %d; want 0", len(got))
