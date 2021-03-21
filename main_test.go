e4b8fdeb0e2fe01c046f23fe69292e4a53a39ed0
Lars Ti <lars.tilsner@googlemail.com>
2021-03-21 18:13:20 +0100

    add some tests regarding env

--- main_test.go
+++ main_test.go
@@ -1,10 +1,40 @@
 package main
 import(
 	"testing"
+	"os"
 )
-func TestReadEnv(t *testing.T) {
+func checkEnv(expected int, t *testing.T){
 	got := readEnv()
-	if len(got) != 0 {
-		t.Errorf("len(got) = %d; want 0", len(got))
+	if len(got) != expected {
+		t.Errorf("len(got) = %d; want %d", len(got), expected)
 	}
 }
+func TestReadEnvEmpty(t *testing.T) {
+	checkEnv(0, t)
+}
+func TestReadEnv1(t *testing.T) {
+	os.Setenv("DEFAULT_1", "free")
+	os.Setenv("SECRET_1", "randomsecret")
+	os.Setenv("STATUS_1", "test")
+	checkEnv(1, t)
+}
+func TestReadEnvMissingSecret(t *testing.T) {
+	os.Setenv("DEFAULT_1", "free")
+	//os.Setenv("SECRET_1", "randomsecret")
+	os.Setenv("STATUS_1", "test")
+	checkEnv(0, t)
+}
+func TestReadEnvMissingStatus(t *testing.T) {
+	os.Setenv("DEFAULT_1", "free")
+	os.Setenv("SECRET_1", "randomsecret")
+	//os.Setenv("STATUS_1", "test")
+	checkEnv(0, t)
+}
+func TestReadEnvMissingDefault(t *testing.T) {
+	//os.Setenv("DEFAULT_1", "free")
+	os.Setenv("SECRET_1", "randomsecret")
+	os.Setenv("STATUS_1", "test")
+	checkEnv(1, t)
+}
+
+
