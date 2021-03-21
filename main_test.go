7f782a4ca059e8736682d70eb3d91ed50e116e92
Lars Ti <lars.tilsner@googlemail.com>
2021-03-21 18:20:27 +0100

    add some tests regarding env

--- main_test.go
+++ main_test.go
@@ -5,6 +5,12 @@ import(
 )
 func checkEnv(expected int, t *testing.T){
 	got := readEnv()
+	os.Unsetenv("DEFAULT_1")
+	os.Unsetenv("SECRET_1")
+	os.Unsetenv("STATUS_1")
+	os.Unsetenv("DEFAULT_2")
+	os.Unsetenv("SECRET_2")
+	os.Unsetenv("STATUS_2")
 	if len(got) != expected {
 		t.Errorf("len(got) = %d; want %d", len(got), expected)
 	}
@@ -17,6 +23,7 @@ func TestReadEnv1(t *testing.T) {
 	os.Setenv("SECRET_1", "randomsecret")
 	os.Setenv("STATUS_1", "test")
 	checkEnv(1, t)
+	
 }
 func TestReadEnvMissingSecret(t *testing.T) {
 	os.Setenv("DEFAULT_1", "free")
